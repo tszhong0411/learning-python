637af368f5fb1bb1143f2f84e803150d12589946
tszhong0411 <me@honghong.me>
2024-09-07 10:44:24 +0800

    feat: request api data

---
+++ request-api-data.py
@@ -0,0 +1,27 @@
+"""
+Request API data
+"""
+
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+
+def get_pokemon_info(name: str):
+    url = f"{base_url}pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to get data. Status code: {response.status_code}")
+
+
+pokemon_name = "pikachu"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info['name'].capitalize()}")
+    print(f"ID: {pokemon_info['id']}")
+    print(f"Height: {pokemon_info['height']}")
+    print(f"Weight: {pokemon_info['weight']}")
